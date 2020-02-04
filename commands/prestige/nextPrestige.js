0097221177e05c3a8e45ef654f907fc4edd248fa
DMills94 <davidj-mills@live.co.uk>
2020-02-04 20:09:53 +0000

    Add next prestige command and first skill (Agility)

--- commands/prestige/nextPrestige.js
+++ commands/prestige/nextPrestige.js
@@ -1,4 +1,11 @@
 const { Command } = require('discord.js-commando')
+const { RichEmbed } = require('discord.js')
+const SKILLS = require('../../skills/skills/skills.js')
+const prestigeFuncs = require('../../skills/prestige-info/prestige.js')
+
+const { stripIndent } = require('common-tags')
+
+const skillList = Object.values(SKILLS).map(skill => skill.info.name)
 
 module.exports = class NextPrestige extends Command {
     constructor(client) {
@@ -8,11 +15,65 @@ module.exports = class NextPrestige extends Command {
             group: 'prestige',
             description: 'Calculates how many actions you need for different skills to reach the next prestige',
             memberName: 'prestige',
-            argsType: 'multiple'
+            args: [
+                {
+                    key: 'skill',
+                    label: 'skill',
+                    prompt: 'What skill you would like to calculate for?',
+                    type: 'string',
+                    error: `
+                        The skill you entered is invalid! Please type one of:
+                        \`${skillList.join(', ')}\`
+                    `,
+                    oneOf: skillList.map(skill => skill.toLowerCase()),
+                    parse: val => val.toLowerCase()
+                },
+                {
+                    key: 'prestige',
+                    label: 'current prestige',
+                    prompt: 'What is your current prestige?',
+                    type: 'integer',
+                    min: 0,
+                    max: 8
+                },
+                {
+                    key: 'xp',
+                    label: 'current xp',
+                    prompt: 'What is your current xp in your prestige?',
+                    type: 'string'
+                }
+            ],
+            argsType: 'multiple',
+            examples: ['nextprestige agility 0 13m']
         })
     }
 
     async run(msg, args) {
-        return msg.channel.send(args.join(' '))
+        let { skill = skill.toLowerCase(), prestige, xp } = args
+        if (['m', 'k'].some(char => xp.toLowerCase().includes(char))) {
+            if (xp.includes('m')) xp = xp.replace('m', '000000')
+            if (xp.includes('k')) xp = xp.replace('k', '000')
+        }
+
+        const { info, methods } = SKILLS[skill]
+        const xpRate = prestigeFuncs.xpRate(prestige)
+        const xpTillPrestige = 200000000 - +xp
+        
+        let embed = new RichEmbed()
+            .setTitle(info.name)
+            .setColor(info.colour)
+            .setThumbnail(info.imageUrl)
+            .setDescription(`You need to gain ${xpTillPrestige.toLocaleString('en')} to reach P${prestige + 1} in ${info.name}`)
+    
+        methods.forEach(method => {
+            const xpPerAction = prestigeFuncs.xpPerAction(method.xp, xpRate)
+            embed.addField(
+                method.name,
+                `XP per action: ${xpPerAction}
+                Actions for prestige: ${Math.ceil(xpTillPrestige / xpPerAction).toLocaleString('en')}`
+            )
+        })
+
+        return msg.channel.send({ embed })
     }
 }
--- skills/prestige-info/prestige.js
+++ skills/prestige-info/prestige.js
@@ -2,4 +2,18 @@ const baseXp = 76
 
 let prestigeFuncs = module.exports = {}
 
+/**
+ * Calculates xp rate for the given prestige
+ * 
+ * @param {number} prestige The prestige to calculate xp rates for
+ */
 prestigeFuncs.xpRate = prestige => baseXp / Math.pow(2, prestige)
+
+/**
+ * Calculates the xp per action, given the xp rate
+ * 
+ * @param {number} actionXp
+ * @param {number} xpRate
+ * @returns {string}
+ */
+prestigeFuncs.xpPerAction = (actionXp, xpRate) => (actionXp * xpRate).toFixed(1).toLocaleString('en')
