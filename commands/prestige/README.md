7b56016274334b232a44837e1635592e78e805a9
DMills94 <davidj-mills@live.co.uk>
2020-02-05 18:00:50 +0000

    Add read me and next 99 infrastructure

---
+++ commands/prestige/README.md
@@ -0,0 +1,11 @@
+If you want to run this yourself clone it and make a `config.json` in the root of the project.
+
+Structure it as
+```
+{
+    "ownerID": "YOUR ID",
+    "token": "YOUR TOKEN"
+}
+```
+
+Run `node bot.js` and you're good
---
+++ commands/prestige/next99.js
@@ -0,0 +1,49 @@
+const { Command } = require('discord.js-commando')
+const SKILLS = require('../../skills/skills/skills.js')
+const { RichEmbed } = require('discord.js')
+
+const skillList = Object.values(SKILLS).map(skill => skill.info.name)
+
+module.exports = class Next99 extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'next99',
+            aliases: ['n99', '99'],
+            group: 'prestige',
+            description: 'Calculates how many actions you need for different skills to reach level 99 in the next prestige',
+            memberName: '99',
+            args: [
+                {
+                    key: 'skill',
+                    label: 'skill',
+                    prompt: 'What skill you would like to calculate for?',
+                    type: 'string',
+                    error: `
+                        The skill you entered is invalid! Please type one of:
+                        \`${skillList.join(', ')}\`
+                    `,
+                    oneOf: skillList.map(skill => skill.toLowerCase()),
+                    parse: val => val.toLowerCase()
+                },
+                {
+                    key: 'prestige',
+                    label: 'current prestige',
+                    prompt: 'What is your current prestige?',
+                    type: 'integer',
+                    min: 0,
+                    max: 8
+                },
+                {
+                    key: 'xp',
+                    label: 'current xp',
+                    prompt: 'What is your current xp in your prestige?',
+                    type: 'string'
+                }
+            ]
+        })
+    }
+
+    async run(msg, args) {
+        msg.channel.send('yes boss')
+    }
+}
