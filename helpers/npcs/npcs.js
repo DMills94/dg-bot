26e4db055db1d47afb15a07df849ea15784d6199
DMills94 <davidj-mills@live.co.uk>
2020-02-08 16:35:09 +0000

    Added a load more bosses innit

--- helpers/npcs/npcs.js
+++ helpers/npcs/npcs.js
@@ -1,13 +1,33 @@
 const bandos = require('./drop-rates/bandos.json')
+const chaosele = require('./drop-rates/chaosele.json')
+const corp = require('./drop-rates/corp.json')
+const kbd = require('./drop-rates/kbd.json')
+const kq = require('./drop-rates/kq.json')
 const kreearra = require('./drop-rates/kreearra.json')
 const kril = require('./drop-rates/kril.json')
+const mole = require('./drop-rates/mole.json')
 const nex = require('./drop-rates/nex.json')
+const prime = require('./drop-rates/prime.json')
+const qbd = require('./drop-rates/qbd.json')
+const rex = require('./drop-rates/rex.json')
+const supreme = require('./drop-rates/supreme.json')
+const wildywyrm = require('./drop-rates/wildywyrm.json')
 const zilyana = require('./drop-rates/zilyana.json')
 
 module.exports = {
   bandos,
+  chaosele,
+  corp,
+  kbd,
+  kq,
   kreearra,
   kril,
+  mole,
   nex,
+  prime,
+  qbd,
+  rex,
+  supreme,
+  wildywyrm,
   zilyana
 }
