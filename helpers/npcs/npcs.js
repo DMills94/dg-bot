a3347b0e104aa9cde1d4efd18e6a103e2728eb53
DMills94 <davidj-mills@live.co.uk>
2020-02-08 15:57:37 +0000

    Finish GWD bosses

--- helpers/npcs/npcs.js
+++ helpers/npcs/npcs.js
@@ -1,5 +1,13 @@
 const bandos = require('./drop-rates/bandos.json')
+const kreearra = require('./drop-rates/kreearra.json')
+const kril = require('./drop-rates/kril.json')
+const nex = require('./drop-rates/nex.json')
+const zilyana = require('./drop-rates/zilyana.json')
 
 module.exports = {
-  bandos
+  bandos,
+  kreearra,
+  kril,
+  nex,
+  zilyana
 }
