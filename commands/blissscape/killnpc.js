ed91045b7694529d6f4016fc0b6f831cc5a6c215
DMills94 <davidj-mills@live.co.uk>
2020-02-08 15:44:00 +0000

    Add boss drop command + bandos

---
+++ commands/blissscape/killnpc.js
@@ -0,0 +1,76 @@
+const { Command } = require('discord.js-commando')
+const { stripIndents } = require('common-tags')
+const NPCS = require('../../helpers/npcs/npcs.js')
+
+const npcList = Object.values(NPCS).map(npc => npc.info.name)
+
+module.exports = class KillNPC extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'killnpc',
+      aliases: ['kb', 'kill'],
+      memberName: 'killnpc',
+      group: 'blissscape',
+      description: 'Simulate killing an npc `x` times',
+      args: [
+        {
+          key: 'npc',
+          label: 'NPC',
+          prompt: 'Which NPC do you want to kill?',
+          type: 'string',
+          error: `
+            I haven't got drop records for that NPC yet, please enter one of:
+            \`${npcList.join(', ')}\`
+          `,
+          oneOf: npcList.map(npc => npc.toLowerCase()),
+          parse: val => val.toLowerCase()
+        },
+        {
+          key: 'amount',
+          label: 'Number of kills',
+          prompt: 'How many do you want to kill?',
+          type: 'integer',
+          min: 1,
+        }
+      ]
+    })
+  }
+
+  async run(msg, args) {
+    const { npc = npc.toLowerCase, amount } = args
+
+    const { info, dropTable } = NPCS[npc]
+    const loot = {}
+    
+    for (let i = 0; i < amount; i++) {
+      const roll = Math.random()
+      let baseline = 0
+
+      for (let [j, item] of dropTable.entries()) {
+        const dropRate = 1 / item.dropRate
+
+        // Hit item
+        if (baseline <= roll && roll <= (baseline + dropRate)) {
+          loot[item.name]
+            ? loot[item.name]++
+            : loot[item.name] = 1
+          break
+        }
+        else {
+          baseline += dropRate
+        }
+      }
+    }
+
+    const formattedLoot = Object.entries(loot).map(item => {
+      return `**${item[1]}x** ${item[0]}`
+    }).join('\n')
+
+
+    msg.channel.send(
+      stripIndents`**__You killed ${amount} ${info.fullName}'s and received:__**
+
+      ${formattedLoot}`
+    )
+  }
+}
---
+++ helpers/npcs/npcs.js
@@ -0,0 +1,5 @@
+const bandos = require('./drop-rates/bandos.json')
+
+module.exports = {
+  bandos
+}
